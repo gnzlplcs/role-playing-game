f0099dc3569f9837906e8e3c96a9a1407844f0f6
gnzlplcs <palaciosgm@outlook.com>
2023-12-22 04:11:30 -0500

    step 39 completed

--- script.js
+++ script.js
@@ -21,3 +21,8 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+
+// functions
+function goStore() {
+  
+};
