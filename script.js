383850244f3ba0821ad43f2ac61388797951e263
gnzlplcs <palaciosgm@outlook.com>
2024-01-06 05:06:12 -0500

    steps 129 - 132 complete

--- script.js
+++ script.js
@@ -171,6 +171,16 @@ function attack() {
   }
 }
 
-function dodge() {}
-function defeatMonster() {}
+function dodge() {
+  text.innerText = "You dodge the attack from the " + monsters[fighting].name;
+}
+
+function defeatMonster() {
+  gold += Math.floor(monsters[fighting].level * 6.7);
+  xp += monsters[fighting].level;
+  goldText.innerText = gold;
+  xpText.innerText = xp;
+  update(locations[4]);
+}
+
 function lose() {}
